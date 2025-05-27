85e295bc008db040e67d33ac4712629f65692864
Arturo Quiroga <arturoqu@microsoft.com>
2025-05-27 10:24:10 -0400

    first commit

---
+++ pushtorepo.sh
@@ -0,0 +1,7 @@
+echo "# ITI-CA" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Arturo-Quiroga-MSFT/ITI-CA.git
+git push -u origin main
